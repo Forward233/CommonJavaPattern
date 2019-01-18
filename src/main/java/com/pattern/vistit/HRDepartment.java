8a2f715dc9f1132449a12715b6a58ff8c54e878b
Forward233 <18729041176@163.com>
2019-01-18 11:50:46 +0800

    init

---
+++ src/main/java/com/pattern/vistit/HRDepartment.java
@@ -0,0 +1,17 @@
+package com.pattern.vistit;
+
+public class HRDepartment extends Department{
+
+	@Override
+	public void Visit(FullTimeEmployee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Visit(PartTimeEmployee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
