4ce486b70a0f82e381a0069416b69c81541202c2
Forward233 <18729041176@163.com>
2020-07-01 08:02:15 +0800

    event

--- src/main/java/com/pattern/observe/two/event/ContextStartEvent.java
+++
@@ -1,9 +0,0 @@
-package com.pattern.observe.two.event;
-
-/**
- * @author: yhl
- * @DateTime: 2020/6/29 7:27
- * @Description:
- */
-public class ContextStartEvent extends AbstractContextEvent {
-}
--- src/main/java/com/pattern/observe/two/listener/EventListener.java
+++
@@ -1,9 +0,0 @@
-package com.pattern.observe.two.listener;
-
-/**
- * @author: yhl
- * @DateTime: 2020/6/29 7:29
- * @Description:
- */
-public interface EventListener {
-}
